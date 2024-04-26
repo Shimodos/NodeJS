be60e247dd29bff8173285d3093d498c4af965ec
Oleksiy Demchenko <ukrartdesign@gmail.com>
2024-04-26 19:10:49 +0300

    3:17: Events

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
-console.log(__filename);
-console.log(__dirname);
-console.log(global);
-console.log(performance);
-console.log(module);
-console.log(exports);
-console.log(require);
+const EventEmitter = require('events');
+
+const myEmitter = new EventEmitter();
+
+const liogDbConnect = () => {
+  console.log('DB connected');
+};
+
+myEmitter.addListener('dbConnected', liogDbConnect);
+myEmitter.emit('dbConnected');
+
+myEmitter.removeListener('dbConnected', liogDbConnect);
+myEmitter.emit('dbConnected');
+
+myEmitter.on('msg', (data) => {
+  console.log(`Message: ${data}`);
+});
+
+myEmitter.prependListener('msg', (data) => {
+  console.log(`Prepended Message: ${data}`);
+});
+
+myEmitter.emit('msg', 'Hello World');
+
+myEmitter.once('msgOnce', () => {
+  console.log('Message Once');
+});
+
+myEmitter.emit('msgOnce');
+myEmitter.emit('msgOnce');
+
+console.log(myEmitter.getMaxListeners());
+myEmitter.setMaxListeners(1);
+console.log(myEmitter.getMaxListeners());
+console.log(myEmitter.listenerCount('msg'));
+console.log(myEmitter.listenerCount('msgOnce'));
+console.log(myEmitter.listeners('msg'));
+console.log(myEmitter.eventNames());
+
+//Error Event
+
+myEmitter.on('error', (err) => {
+  console.log(`Error: ${err.message}`);
+});
+
+myEmitter.emit('error', new Error('Something went wrong'));
+
+const target = new EventTarget();
+
+const logTarget = () => {
+  console.log('EventTarget');
+};
+
+target.addEventListener('log', logTarget);
+target.dispatchEvent(new Event('log'));
