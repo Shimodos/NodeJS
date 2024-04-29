f0ab923756ddc8a024521622b4c82061f1ac4e3e
Oleksiy Demchenko <ukrartdesign@gmail.com>
2024-04-29 23:50:47 +0300

    4:23: performance measurement

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-// const crypto = require('crypto');
-const https = require('https');
-const start = performance.now();
+const perf_hooks = require('perf_hooks');
 
-// process.env.UV_THREADPOOL_SIZE = 8;
+test = perf_hooks.performance.timerify(test); // Wrapping the function with timerify
 
-// for (let i = 0; i < 50; i++) {
-//   crypto.pbkdf2('test', 'salt', 100000, 64, 'sha512', () => {
-//     console.log(performance.now() - start);
-//   });
-// }
+const PerformanceObserver = new perf_hooks.PerformanceObserver((items, observer) => {
+  console.log(items.getEntries());
+  const entry = items.getEntries('slow').pop();
+  console.log(`The time taken is ${entry.name}  ${entry.duration}`);
+  observer.disconnect();
+});
 
-for (let i = 0; i < 50; i++) {
-  https.get('https://www.google.com', (res) => {
-    res.on('data', () => {});
-    res.on('end', () => {
-      console.log(performance.now() - start);
-    });
-  });
+PerformanceObserver.observe({
+  entryTypes: ['measure', 'function'],
+  buffered: true,
+});
+
+function test() {
+  const arr = [];
+  for (let i = 0; i < 1000000; i++) {
+    arr.push(i * i);
+  }
 }
+
+function slow() {
+  performance.mark('start');
+  const arr = [];
+  for (let i = 0; i < 7000000; i++) {
+    arr.push(i * i);
+  }
+  performance.mark('end');
+  performance.measure('slow', 'start', 'end');
+
+  // console.log(performance.getEntriesByName('slow'));
+}
+
+slow();
+test();
