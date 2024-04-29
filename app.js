edc9ef3dbd91924c69588890783b76dd2ebf0af8
Oleksiy Demchenko <ukrartdesign@gmail.com>
2024-04-29 23:36:05 +0300

    4:22: Worker threads

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const a = 5;
+// const crypto = require('crypto');
+const https = require('https');
+const start = performance.now();
 
-function b() {
-  return c();
+// process.env.UV_THREADPOOL_SIZE = 8;
+
+// for (let i = 0; i < 50; i++) {
+//   crypto.pbkdf2('test', 'salt', 100000, 64, 'sha512', () => {
+//     console.log(performance.now() - start);
+//   });
+// }
+
+for (let i = 0; i < 50; i++) {
+  https.get('https://www.google.com', (res) => {
+    res.on('data', () => {});
+    res.on('end', () => {
+      console.log(performance.now() - start);
+    });
+  });
 }
-
-function c() {
-  return d();
-}
-
-function d() {
-  console.log(a);
-}
-
-setTimeout(() => {
-  console.log('Hello World!');
-}, 1000);
-
-b();
