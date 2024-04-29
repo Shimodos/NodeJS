9c147dde08871df757ab54b6823c196a1fdbef52
Oleksiy Demchenko <ukrartdesign@gmail.com>
2024-04-29 23:19:55 +0300

    4:21: Stack

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
-const fs = require('fs');
+const a = 5;
 
-console.log('Init');
+function b() {
+  return c();
+}
 
-setTimeout(() => {
-  console.log(performance.now(), 'Timeout 0');
-}, 100);
+function c() {
+  return d();
+}
 
-setImmediate(() => {
-  console.log('Immediate 0');
-});
-
-fs.readFile(__filename, () => {
-  console.log('FS');
-});
+function d() {
+  console.log(a);
+}
 
 setTimeout(() => {
-  for (let i = 0; i < 1000000000; i++) {}
-  console.log('done 1');
+  console.log('Hello World!');
+}, 1000);
 
-  Promise.resolve().then(() => {
-    console.log('Promise 2');
-  });
-
-  process.nextTick(() => {
-    console.log('NextTick 1');
-  });
-}, 0);
-
-Promise.resolve().then(() => {
-  console.log('Promise 1');
-});
-
-process.nextTick(() => {
-  console.log('NextTick 0');
-});
-
-console.log('Final');
+b();
