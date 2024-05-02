a2867cce5c06c991e8795a9bee7ebb85be861511
Oleksiy Demchenko <ukrartdesign@gmail.com>
2024-05-02 21:15:26 +0300

    6:29: V8

--- app.js
+++ app.js
@@ -1,51 +1,12 @@
-const { performance, PerformanceObserver } = require('perf_hooks'); // Импорт модуля для работы с производительностью
-const { Worker } = require('worker_threads'); // Импорт модуля для работы с потоками
-const { fork } = require('child_process'); // Импорт модуля для работы с процессами
-
-const perfObserver = new PerformanceObserver((list) => {
-  list.getEntries().forEach((entry) => {
-    console.log(`${entry.name}, ${entry.duration}`);
-  });
-});
-
-perfObserver.observe({
-  entryTypes: ['measure'],
-  buffered: true,
-});
-
-const workerFunction = (array) => {
-  return new Promise((resolve, reject) => {
-    performance.mark('workerStart');
-
-    const worker = new Worker('./worker.js', { workerData: { array } });
-
-    worker.on('message', (msg) => {
-      performance.mark('workerEnd');
-      performance.measure('worker:', 'workerStart', 'workerEnd');
-
-      resolve(msg);
-    });
-  });
+let outter = null;
+let run = function () {
+  let inner = outter;
+  let unused = function () {
+    if (inner) console.log(`inner`);
+  };
+  outter = {
+    logStr: new Array(1000000).join('x'),
+  };
 };
 
-const forkFunction = (array) => {
-  return new Promise((resolve, reject) => {
-    performance.mark('forkStart');
-
-    const forkProccess = fork('./fork.js');
-    forkProccess.send({ array });
-    forkProccess.on('message', (msg) => {
-      performance.mark('forkEnd');
-      performance.measure('fork:', 'forkStart', 'forkEnd');
-
-      resolve(msg);
-    });
-  });
-};
-
-const main = async () => {
-  await workerFunction([1, 2, 3, 4, 5]);
-  await forkFunction([1, 2, 3, 4, 5]);
-};
-
-main();
+setInterval(run, 1000);
