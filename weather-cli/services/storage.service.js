5410753d157ec82b377aa7626f0ae6d2da32dbe5
Oleksiy Demchenko <ukrartdesign@gmail.com>
2024-05-06 23:31:27 +0300

    8:39: Working with the file system

--- weather-cli/services/storage.service.js
+++ weather-cli/services/storage.service.js
@@ -1,9 +1,37 @@
-import { dir } from 'console';
 import { homedir } from 'os';
 import { join } from 'path';
+import { promises } from 'fs';
 
 const filePath = join(homedir(), 'weather-cli.json');
 
-const saveKeyValue = (key, value) => {};
+const saveKeyValue = async (key, value) => {
+  let data = {};
 
-export { saveKeyValue };
+  if (await isExists(filePath)) {
+    const file = await promises.readFile(filePath, 'utf-8');
+    data = JSON.parse(file);
+  }
+
+  data[key] = value;
+  await promises.writeFile(filePath, JSON.stringify(data), 'utf-8');
+};
+
+const getKeyValues = async () => {
+  if (await isExists(filePath)) {
+    const file = await promises.readFile(filePath, 'utf-8').catch((error) => console.error(error));
+    const data = JSON.parse(file);
+    return data[key];
+  }
+  return undefined;
+};
+
+const isExists = async (path) => {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export { saveKeyValue, getKeyValues };
--- weather-cli/weather.js
+++ weather-cli/weather.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printHelp, prinSuccess } from './services/log.service.js';
+import { printHelp, prinSuccess, printError } from './services/log.service.js';
 import { saveKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+  try {
+    await saveKeyValue('token', token);
+    prinSuccess('Token already saved');
+  } catch (error) {
+    printError(error.message);
+  }
+};
 
 const initCLI = () => {
   const args = getArgs(process.argv);
@@ -15,7 +24,7 @@ const initCLI = () => {
   }
   if (args.t) {
     // Token
-    saveKeyValue('token', args.t);
+    return saveToken(args.t);
   }
 };
 
