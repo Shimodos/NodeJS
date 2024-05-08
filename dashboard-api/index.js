9af1745748eb5c244e58f062ce723ded21ae5a81
Oleksiy Demchenko <ukrartdesign@gmail.com>
2024-05-08 22:30:38 +0300

    9:49: Routing

--- dashboard-api/index.js
+++ dashboard-api/index.js
@@ -4,8 +4,26 @@ const host = '127.0.0.1';
 const port = 3000;
 const app = express();
 
-app.get('/Helloworld', (req, res) => {
-  res.send('Hello World');
+app.all('/Helloworld', (req, res, next) => {
+  console.log('Accessing the secret section ...');
+  next();
+});
+
+const cb = (req, res, next) => {
+  console.log('CB1');
+};
+
+app
+  .route('/user')
+  .get('/Helloworld', cb, (req, res) => {
+    res.send('Hello World');
+  })
+  .post('/Helloworld', cb, (req, res) => {
+    res.send('Hello World1');
+  });
+
+app.post('/Helloworld', cb, (req, res) => {
+  res.send('Hello World2');
 });
 
 app.listen(port, host, () => {
