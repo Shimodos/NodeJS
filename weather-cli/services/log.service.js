b8e3ac6cb6bc7f7f6260f713686c1c1c3dac8b7b
Oleksiy Demchenko <ukrartdesign@gmail.com>
2024-05-09 13:48:10 +0300

    add: weather-cli, add new elemts

--- weather-cli/services/log.service.js
+++ weather-cli/services/log.service.js
@@ -24,11 +24,19 @@ const printWeather = (res, icon) => {
   console.log(
     dedent`${chalk.bgYellow(' Weather ')}
     City: ${res.name} ${icon}
+    Country: ${res.sys.country}
     Temperature: ${res.main.temp}°C
     Feels like: ${res.main.feels_like}°C
     Humidity: ${res.main.humidity}%
     Description: ${res.weather[0].description}
     Wind speed: ${res.wind.speed} m/s
+    Wind speed: ${(res.wind.speed * 3.6).toFixed(2)} km/h
+    Pressure: ${res.main.pressure} hPa
+    Visibility: ${res.visibility} m
+    Rain: ${res.rain ? res.rain['1h'] : 0} mm
+    Sunrise: ${new Date(res.sys.sunrise * 1000).toLocaleTimeString()}
+    Sunset: ${new Date(res.sys.sunset * 1000).toLocaleTimeString()}
+    
     `,
   );
 };
