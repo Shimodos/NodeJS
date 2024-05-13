76d18ff52292f1b5e0cdaed3091aed73088fecc1
Aleksey Demchenko <ad@pesochin.net>
2024-05-13 20:20:11 +0300

    10:63: typeof, keyoff, Null, Void

--- dashboard-api/test.ts
+++ dashboard-api/test.ts
@@ -1,102 +1,36 @@
-class Coord {
+// Typeof operator
+let a = 'Hello World';
+
+if (typeof a === 'string') {
+  console.log(a);
+}
+
+let b: typeof a = 'Hello World';
+
+// Keyof operator
+type Coord = {
   lat: number;
-  lon: number;
+  long: number;
+};
 
-  protected test() {
-    if (this.lat > 0) {
-      console.log('Lat is positive');
-    } else {
-      console.log('Lat is negative');
-    }
-  }
+type P = keyof Coord;
 
-  computeDistance(newLat: number, newLon: number) {
-    this.test();
-    return 0;
-  }
+let c: P = 'lat';
 
-  constructor(lat: number, lon: number) {
-    this.lat = lat;
-    this.lon = lon;
+// Null and undefined
+function log(a: string | null) {
+  if (a === null) {
+    console.log('a is null');
+  } else {
+    a.toLowerCase();
   }
 }
 
-const point = new Coord(0, 1);
-point.lat = 10;
-point.lon = 20;
-
-class MapLocathion extends Coord {
-  private _name: string;
-
-  get name() {
-    return this._name;
-  }
-
-  set name(value: string) {
-    this._name = value + '!';
-  }
-
-  override computeDistance(newLat: number, newLon: number) {
-    console.log(this._name);
-    this.test();
-    return 0;
-  }
-
-  constructor(lat: number, lon: number, name: string) {
-    super(lat, lon);
-    this._name = name;
-  }
+//Void
+function log2(a: string): void {
+  console.log(a);
 }
 
-const loc = new MapLocathion(0, 1, 'Test');
-loc.name = 'Test';
-
-interface LoggerService {
-  log(s: string): void;
-}
-
-class Logger implements LoggerService {
-  public log(s: string) {
-    console.log(s);
-  }
-
-  private error(s: string) {}
-}
-
-const l = new Logger();
-l.log('Hello');
-// l.error('Error'); // Error: Property 'error' is private and only accessible within class 'Logger'.
-
-class MyClass<T> {
-  a: T;
-}
-
-const b = new MyClass<string>();
-b.a = 'Hello';
-
-abstract class AbstractClass {
-  print(s: string) {
-    console.log(s);
-  }
-
-  abstract test(s: string): void;
-}
-
-class AbstractClassImpl extends AbstractClass {
-  test(s: string) {
-    // console.log(s);
-  }
-}
-
-new AbstractClassImpl().print('Hello');
-
-class Animal {
-  name: string;
-}
-
-class Dog extends Animal {
-  name: string;
-  breed: string;
-}
-
-const puppy: Animal = new Dog();
+const d: bigint = BigInt(100);
+const e: unique symbol = Symbol('foo');
+const f: symbol = Symbol('bar');
