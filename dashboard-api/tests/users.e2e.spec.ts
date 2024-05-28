72759a9dbcf716b5452d7878c7e38d67919e6d25
Aleksey Demchenko <ad@pesochin.net>
2024-05-29 00:17:50 +0300

    17:98: e2e Tests 001

--- dashboard-api/tests/users.e2e.spec.ts
+++ dashboard-api/tests/users.e2e.spec.ts
@@ -16,6 +16,41 @@ describe('Users e2e', () => {
 			.send({ email: 'sdd@sas1.com', password: '1234' });
 		expect(res.status).toEqual(422);
 	});
+
+	//test login
+
+	it('Login - succes', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'sdd@sas1.com', password: 'dhghfgdfg' });
+		expect(res.body.token).not.toBeUndefined();
+	});
+
+	it('Login - error', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'sdd@sas1.com', password: '1' });
+		expect(res.statusCode).toBe(401);
+	});
+
+	// test info
+
+	it('Info - succes', async () => {
+		const login = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'sdd@sas1.com', password: 'dhghfgdfg' });
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${login.body.token}`);
+		expect(res.body.email).toBe('sdd@sas1.com');
+	});
+
+	it('Info - error', async () => {
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${1}`);
+		expect(res.statusCode).toBe(401);
+	});
 });
 
 afterAll(() => {
